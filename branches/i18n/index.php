b5889478a6da81b83ea7bbcc43c4af0e35e36c5f
david.danier <david.danier@a80a18ea-1906-11de-b69a-45dc1c5e4070>
2009-03-27 11:45:49 +0000

    * Example for translate_template()

--- branches/i18n/index.php
+++ branches/i18n/index.php
@@ -7,24 +7,7 @@ include("header.php");
    
 <div class="left">
 	<div class="message">
-
-		<p>
-            <b>
-			Dieser Service bietet eine dezente und unaufdringliche Möglichkeit, Ihre Besucher darüber zu
-            informieren, dass sie zu einem neueren Browser wechseln sollten.
-            </b>
-		</p>
-		<p>
-            Viele Internetbenutzer verwenden sehr alte und überholte Browser
-            (z.B. den mittlerweile acht Jahre alten <b>Internet Explorer 6</b>) – meist ohne wirklichen Grund.
-
-            Der Wechsel zu einem neueren Browser bietet für den Benutzer und für Sie als Webdesigner viele Vorteile.
-			
-			<!--They just have to be told, that using an old browser gives them disadvantages.-->
-		</p>
-		<!--<p>
-			The more Websites Participate, the more the awareness to update will increase and old browser market share will drop faster!
-		</p>-->
+		<?php translate_template('index-teaser.php'); ?>
 	</div>
 
 		<h2>Wie funktioniert's?</h2>
---
+++ branches/i18n/lang/en/index-teaser.php
@@ -0,0 +1,13 @@
+		<p>
+			<b>
+			This service is an 	unobtrusive opportunity to inform your visitors to switch to a newer browser.
+			</b>
+		</p>
+		<p>
+			Many of your visitors are still using very old, outdated browsers, most of them for no real reason. Switching to an newer browser is better for them and for you as a webdesigner. 
+			
+			<!--They just have to be told, that using an old browser gives them disadvantages.-->
+		</p>
+		<!--<p>
+			The more Websites Participate, the more the awareness to update will increase and old browser market share will drop faster!
+		</p>-->
