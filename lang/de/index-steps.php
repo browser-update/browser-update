538a00c478bb8dcca0140bee2e968f670c8e8be9
kb@amxn.com <kb@amxn.com@a80a18ea-1906-11de-b69a-45dc1c5e4070>
2009-04-07 19:17:38 +0000

    Implemented suggestions of issue 27

--- lang/de/index-steps.php
+++ lang/de/index-steps.php
@@ -1,22 +1,36 @@
-		<ol class="steps">
-			<li><div>
-                Javascript-Benachrichtigungscode in Webseite <a href="#install">einbinden</a>
-                </div></li>
-			<li><div>
-                Besucher mit veralteten Browsern erhalten eine unaufdringliche Meldung, dass
-                ihr Browser nicht mehr aktuell ist und es besser ist, auf eine neuere
-                Version zu aktualisieren.
-                (<a href="#" onclick="$bu=new $buo({},true);">Benachrichtigung testen!</a>)<br/>
-                <a href="#" onclick="$bu=new $buo({},true);"><img src="/img/bar-small.png" alt="" style="padding-top:6px;"/></a>
-                </div></li>
-			<li><div>
-                Durch Anklicken der Meldung gelangt der Besucher auf eine
-                <a href="update.html">Seite mit Argumenten für ein Update
-                und einer Auswahl aktueller Browser.</a>
-			</div></li>
-			<li><div>
-                Keine Bevormundung &ndash; Der Besucher muss der Empfehlung nicht folgen
-                und wird dann längere Zeit von einer erneuten
-                Benachrichtigung verschont.
-			</div></li>
-        </ol>
+<ol class="steps">
+	<li>
+		<div>
+			Javascript-Benachrichtigungscode in Webseite <a href="#install">einbinden</a>
+		</div>
+	</li>
+	<li>
+		<div>
+			Besucher mit veralteten Browsern erhalten eine unaufdringliche Meldung, dass
+			ihr Browser nicht mehr aktuell ist und es besser ist, auf eine neuere
+			Version zu aktualisieren.
+			(<a href="#" onclick="$bu=new $buo({},true);">Benachrichtigung testen!</a>)<br/>
+			<a href="#" onclick="$bu=new $buo({},true);"><img src="/img/bar-small.png" alt="" style="padding-top:6px;"/></a>
+			<div class="example">
+				Sie verwenden einen <strong>veralteten Browser</strong> (Internet Explorer 6)
+				mit <strong>Sicherheitsschwachstellen</strong> und <strong>können nicht alle
+				Funktionen dieser Webseite nutzen</strong>.
+				<a href="#" onclick="$bu=new $buo({},true);">Hier erfahren Sie, wie einfach Sie Ihren Browser aktualisieren können</a>
+			</div>
+		</div>
+	</li>
+	<li>
+		<div>
+			Durch Anklicken der Meldung gelangt der Besucher auf eine
+			<a href="update.html">Seite mit Argumenten für ein Update
+			und einer Auswahl aktueller Browser.</a>
+		</div>
+	</li>
+	<li>
+		<div>
+			Keine Bevormundung &ndash; Der Besucher muss der Empfehlung nicht folgen
+			und wird dann längere Zeit von einer erneuten
+			Benachrichtigung verschont.
+		</div>
+	</li>
+</ol>
--- lang/en/index-steps.php
+++ lang/en/index-steps.php
@@ -1,20 +1,34 @@
-		<ol class="steps">
-			<!--<li><div>You should (of course) code the Website that it is accessible by old webbrowsers, but you can leave some features/gimmicks out.</div></li>-->
-			<li><div>
-                Include our small javascript notification on your website
-            </div></li>
-			<li><div>
-                Visitors with out-dated browser will be informed by a little,
-                undisturbing bar, that his browser is not up-to-date and it
-                is recommended to update.
-                (<a href="#" onclick="$buo({},true);">Test the notification bar</a>)<br/>
-                <a href="#" onclick="$bu=new $buo({},true);"><img src="/img/bar-small.png" alt="" style="padding-top:6px;"/></a>
-                <!--, to see all the features of this website.--></div></li>
-			<li><div>
-                By clicking the bar, he will get to <a href="update.html">an info page
-                with arguments why to change/update and some browser choices</a>.
-            </div></li>
-            <li><div>
-                If the visitor ignores the advice, it won't appear again for some time.
-			</div></li>
-		</ol>
+<ol class="steps">
+	<!--<li><div>You should (of course) code the Website that it is accessible by old webbrowsers, but you can leave some features/gimmicks out.</div></li>-->
+	<li>
+		<div>
+			Include our small javascript notification on your website
+		</div>
+	</li>
+	<li>
+		<div>
+			Visitors with out-dated browser will be informed by a little,
+			undisturbing bar, that his browser is not up-to-date and it
+			is recommended to update.
+			(<a href="#" onclick="$buo({},true);">Test the notification bar</a>)<br/>
+			<div class="example">
+				Your browser (Internet Explorer 6) is <strong>out of date</strong>.
+				It has known <strong>security flaws</strong> and may <strong>not
+				display all features</strong> of this and other websites.
+				<a href="#" onclick="$bu=new $buo({},true);">Learn how to update your browser</a>
+			</div>
+			<!--, to see all the features of this website.-->
+		</div>
+	</li>
+	<li>
+		<div>
+			By clicking the bar, he will get to <a href="update.html">an info page
+			with arguments why to change/update and some browser choices</a>.
+		</div>
+	</li>
+	<li>
+		<div>
+			If the visitor ignores the advice, it won't appear again for some time.
+		</div>
+	</li>
+</ol>
