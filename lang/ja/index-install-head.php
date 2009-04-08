7250df24b6f1e159ae012487ad619e726b8f9680
niiconn <niiconn@a80a18ea-1906-11de-b69a-45dc1c5e4070>
2009-04-08 16:20:19 +0000

    Japanese translation (first internal rev): This is a preliminary update. The translation is far from completion.

---
+++ lang/ja/index-install-head.php
@@ -0,0 +1,12 @@
+		
+<p>
+    Binden Sie einfach den hier generierten Code an beliebiger Stelle in Ihre Webseite ein.
+</p>
+<p>
+    代用として、<a href="http://fileserver.julianbrinke.de/WP/">ＷｏｒｄＰｒｅｓｓ</a>や<a href="http://typolight.org/erweiterungsliste/view/browser_update.html">ＴＹＰＯＬｉｇｈｔ</a>をプラグインとして使えます。
+</p>
+<!--
+<p>
+    Sie können den Hinweistext abändern oder die Voreinstellungen beibehalten (empfohlen).
+</p>
+-->
