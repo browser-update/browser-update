213fe1fade191241b37eec446747a66ece46968e
niiconn <niiconn@a80a18ea-1906-11de-b69a-45dc1c5e4070>
2009-04-09 16:32:49 +0000

    Japanese translation: Changed all full width romaji back to half width romaji for better readability.

--- lang/ja/update-browsers.php
+++ lang/ja/update-browsers.php
@@ -3,28 +3,28 @@
 			
 			<ul class="browsers">
 				<li class="ff">
-					<h3><a href="http://www.mozilla.com/firefox/" onmousedown="countBrowser('f')">Ｆｉｒｅｆｏｘ　３</a></h3>
+					<h3><a href="http://www.mozilla.com/firefox/" onmousedown="countBrowser('f')">Firefox 3</a></h3>
 					<div>倍拡張版オープンソースブラウザー、強力な拡張と調整機能 </div>
 					<a href="http://www.mozilla.com/firefox/" onmousedown="countBrowser('f')">ダウンロード</a>
 				</li>
 				<li class="op">
-					<h3><a href="http://www.opera.com/" onmousedown="countBrowser('o')">Ｏｐｅｒａ　９.６</a></h3>
+					<h3><a href="http://www.opera.com/" onmousedown="countBrowser('o')">Opera 9.6</a></h3>
 					<div>Viele integrierte Extrafunktionen</div>
 					<a href="http://www.opera.com/" onmousedown="countBrowser('o')">ダウンロード</a>
 				</li>
 				<li class="sa">
-					<h3><a href="http://www.apple.com/jp/safari/" onmousedown="countBrowser('s')">Ｓａｆａｒｉ 3.2</a></h3>
-					<div>Ａｐｐｌｅ社製のブラウザー</div>
+					<h3><a href="http://www.apple.com/jp/safari/" onmousedown="countBrowser('s')">Safari 3.2</a></h3>
+					<div>Apple社製のブラウザー</div>
 					<a href="http://www.apple.com/jp/safari/" onmousedown="countBrowser('s')">ダウンロード</a>
 				</li>
 				<li class="ch">
-					<h3><a href="http://www.google.com/chrome?hl=ja" onmousedown="countBrowser('c')">Ｇｏｏｇｌｅ　Ｃｈｒｏｍｅ １.０</a></h3>
-					<div>コンパクトサーフェイスブラウザー Ｇｏｏｇｌｅ製</div>
+					<h3><a href="http://www.google.com/chrome?hl=ja" onmousedown="countBrowser('c')">Google　Chrome 1.0</a></h3>
+					<div>コンパクトサーフェイスブラウザー Google製</div>
 					<a href="http://www.google.com/chrome?hl=ja" onmousedown="countBrowser('c')">ダウンロード</a>
 				</li>
 				<li class="ie">
-					<h3><a href="http://www.microsoft.com/japan/windows/products/winfamily/ie/default.mspx" onmousedown="countBrowser('i')">Ｉｎｔｅｒｎｅｔ　Ｅｘｐｌｏｒｅｒ　８</a></h3>
-					<div>Ｗｉｎｄｏｗｓの標準ウェブブラウザー</div>
+					<h3><a href="http://www.microsoft.com/japan/windows/products/winfamily/ie/default.mspx" onmousedown="countBrowser('i')">Internet Explorer 8</a></h3>
+					<div>Windowsの標準ウェブブラウザー</div>
 					<a href="http://www.microsoft.com/japan/windows/products/winfamily/ie/default.mspx" onmousedown="countBrowser('i')">ダウンロード</a>
 				</li>
 			</ul>
