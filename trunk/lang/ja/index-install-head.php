5d9966672d77fd6c90d3b546659ef865f9a81cf3
niiconn <niiconn@a80a18ea-1906-11de-b69a-45dc1c5e4070>
2009-04-09 16:32:49 +0000

    Japanese translation: Changed all full width romaji back to half width romaji for better readability.

--- trunk/lang/ja/index-install-head.php
+++ trunk/lang/ja/index-install-head.php
@@ -3,7 +3,7 @@
     Binden Sie einfach den hier generierten Code an beliebiger Stelle in Ihre Webseite ein.
 </p>
 <p>
-    代用として、<a href="http://fileserver.julianbrinke.de/WP/">ＷｏｒｄＰｒｅｓｓ</a>や<a href="http://typolight.org/erweiterungsliste/view/browser_update.html">ＴＹＰＯＬｉｇｈｔ</a>をプラグインとして使えます。
+    代用として、<a href="http://fileserver.julianbrinke.de/WP/">Wordpress</a>や<a href="http://typolight.org/erweiterungsliste/view/browser_update.html">TYPOLight</a>をプラグインとして使えます。
 </p>
 <!--
 <p>
