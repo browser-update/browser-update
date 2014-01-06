77dcee63c7383fef2963049095c1a5f8901fe2b4
David Danier <ddanier@users.noreply.github.com>
2014-01-06 16:36:15 +0000

    compiled all translations

--- scripts/l10n_compile.php
+++ scripts/l10n_compile.php
@@ -7,6 +7,7 @@ $locale = $_SERVER['argv'][1];
 $dir = dirname(dirname(__FILE__));
 $lang_dir = $dir.DIRECTORY_SEPARATOR.'lang'.DIRECTORY_SEPARATOR.$locale.DIRECTORY_SEPARATOR.'LC_MESSAGES'.DIRECTORY_SEPARATOR;
 chdir($dir);
-exec('msgfmt '.escapeshellarg($lang_dir.'browser-update.po').' -o '.escapeshellarg($lang_dir.'browser-update.mo'));
-
-?>
+exec('msgfmt '.escapeshellarg($lang_dir.'update.po').' -o '.escapeshellarg($lang_dir.'update.mo'));
+exec('msgfmt '.escapeshellarg($lang_dir.'site.po').' -o '.escapeshellarg($lang_dir.'site.mo'));
+exec('msgfmt '.escapeshellarg($lang_dir.'customize.po').' -o '.escapeshellarg($lang_dir.'customize.mo'));
+exec('msgfmt '.escapeshellarg($lang_dir.'update-legacy.po').' -o '.escapeshellarg($lang_dir.'update-legacy.mo'));
