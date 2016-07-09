d7b757a0e4e2febbf4ed3ed585f8a9e7664979bb
josselex <jossele@gmx.de>
2016-07-09 18:32:56 +0200

    a lot of improvements
    * started android notification
    * made browsers to show modular
    * detect on the site if browser is up-to date and show different text

--- update-browser.php
+++ update-browser.php
@@ -32,18 +32,11 @@ if(strlen($lr)!=5) {
 }
 $lr=str_replace("_","-",$lr);
 
-
-
-if (!$newtrans) {
-    header("Location: update-outdated.html");
-    exit;
-}
-
 include("header.php");
 
 $ua=str_replace(array("/","+","_","\n","\t",".")," ", strtolower($_SERVER['HTTP_USER_AGENT']));
 function has($t) {
-	global $ua;	
+	global $ua;
 	return !(strpos($ua,$t)===false);
 }
 
@@ -52,118 +45,107 @@ function has($t) {
 //IE 10 is only available for win7 and win8 || has("windows nt 6.0")
 //IE 11 is only available for win7 and win8 || has("windows nt 6.0")
 // 5->2000/xp/2003 6.0->vista, 6.1->win7, 6.2->win8, 6.3->win 8.1
-$no_ie = has("os x") || has("linux");
+$no_ie = has("os x") || has("linux") || has("android");
 $no_ie_system = has("windows nt 4") || has("windows nt 5") || has("windows nt 6 0")|| has("os x") || has("linux");
 $no_sa = !has("os x");
 $no_sa_system =  has("os x 10 4") || has("os x 10 5") || has("os x 10 6") || has("os x 10 7");
-#echo $no_sa;
-#
+
+
 //$u_sa="http://www.apple.com/safari/";
 $sa_map=array("en"=>"","sv"=>"se","ja"=>"jp","sl"=>"si","uk"=>"ua","rm"=>"de","da"=>"dk","ca"=>"es");
 $sal="https://itunes.apple.com/%s/app/os-x-yosemite/id915041082?mt=12";
-//if (in_array($ll, array("de","es","pl","pt","fr","nl"))) 
+//if (in_array($ll, array("de","es","pl","pt","fr","nl")))
 //    $sal="http://clkuk.tradedoubler.com/click?p=23761&a=2364610&url=https%%3A%%2F%%2Fitunes.apple.com%%2F%s%%2Fapp%%2Fos-x-mavericks%%2Fid675248567%%3Fmt%%3D12%%26uo%%3D4%%26partnerId%%3D2003";
 $u_sa=sprintf($sal,$ll);
 if (isset($sa_map[$ll]))
     $u_sa=sprintf($sal,$sa_map[$ll]);
 
 $u_ff="http://www.mozilla.com/firefox/";
-$u_op="http://www.opera.com/browser/";
 $u_op="http://www.opera.com/?utm_medium=roc&utm_source=team23_de&utm_campaign=browser-update_org";
-#$u_ch=sprintf("http://www.google.com/chrome?hl=%s",$ll);
 $u_ch="https://www.google.com/chrome/browser/desktop/";
 $u_ie=sprintf("http://windows.microsoft.com/%s/internet-explorer/downloads/ie",$lr);
+
+if (has("android")) {
+    $u_ff="https://play.google.com/store/apps/details?id=org.mozilla.firefox";
+    $u_op="www.opera.com/mobile/operabrowser";
+    $u_ch="https://play.google.com/store/apps/details?id=com.android.chrome";
+}
+    
+function brow($name, $url, $vendor, $char, $na=False) {
+    echo '<td class="b b'.$char.'">';
+    if (!$na) {
+        echo '<a class="l" href="'.$url.'" target="_blank" onmousedown="countBrowser(\''.$char.'\')">';
+    }
+    else {
+        echo' <a class="l notavailable">';
+    }
+    echo '<span class="bro">'.$name.'</span>';
+    echo '<span class="vendor">'.$vendor.'</span>';
+    if ($na) {
+        echo '
+        <span class="na">'. T_('Most recent version not available for your system.').' '.
+        T_('Please choose another browser.').'</span>';
+    }
+    echo '
+        </a>
+    </td> 
+    ';
+}
+
+if (!is_outdated() and !(isset($_GET['force_outdated']) and $_GET['force_outdated'])) {
 ?>
-
-
-
+<div class="noti">
+<h2 class="whatnow">
+    <b><?php echo (T_('Your browser is up-to-date.'))?></b>
+</h2>
+</div>
+<?php
+}
+else {
+?>
 
 <div class="noti">
 <h2 class="whatnow">
-    <b><?php echo T_('Your browser is out-of-date. Please download one of these up-to-date, free and excellent browsers:');?></b>
+    <b><?php echo T_('Your browser is out-of-date. Please download one of these up-to-date, free and excellent browsers:'); ?></b>
 </h2>
 
 <table class="logos">
     <tr>
-    <td class="b bf">
-        <a class="l" href="<?php echo $u_ff;?>" target="_blank" onmousedown="countBrowser('f')"><span class="bro">Firefox</span>   
-        <span class="vendor">Mozilla Foundation</span></a>
-    </td>
-    <td class="b bo">
-        <a class="l" href="<?php echo $u_op;?>" target="_blank" onmousedown="countBrowser('o')"><span class="bro">Opera</span>   
-        <span class="vendor">Opera Software</span>  </a>
-    </td>
-    <td class="b bc">
-        <a class="l" href="<?php echo $u_ch;?>" target="_blank" onmousedown="countBrowser('c')"><span class="bro">Chrome</span>   
-        <span class="vendor">Google</span>           </a>
-    </td>
-    <?php if ($no_sa_system) {?>
-    <td class="b bs">
-        <a class="l notavailable"><span class="bro">Safari</span>   
-        <span class="vendor">Apple</span>   
-        <span class="na"><?php echo T_('Most recent version not available for your system.');?> <?php echo T_('Please choose another browser.'); ?> </span>
-        </a>
-    </td>
-    <?php } else if ($no_sa) {?>
-    <?php } else {?>
-    <td class="b bs">
-        <a class="l" href="<?php echo $u_sa;?>" target="_blank" onmousedown="countBrowser('s')"><span class="bro">Safari</span>   
-        <span class="vendor">Apple</span>         </a>
-    </td>
-    <?php } if ($no_ie) {?>
-    <?php } else if ($no_ie_system) {?>
-    <td class="b bi">
-        <a class="l notavailable"><span class="bro">Edge</span>   
-        <span class="vendor">Microsoft</span>   
-        <span class="na"><?php echo T_('Most recent version not available for your system.');?> <?php echo T_('Please choose another browser.'); ?> </span>
-        </a>
-    </td>      
-    <?php } else {?>
-    <td class="b bi">
-        <a class="l" href="<?php echo $u_ie;?>" target="_blank" onmousedown="countBrowser('i')"><span class="bro">Edge</span>   
-        <span class="vendor">Microsoft</span>        </a>
-    </td>
-    <?php }?>
-
+        <?php
+        brow("Firefox",$u_ff,"Mozilla Foundation","f");
+        brow("Opera",$u_op,"Opera Software","o");
+        brow("Chrome",$u_ch,"Google","c");
+        if (!$no_sa)
+            brow("Safari",$u_sa,"Apple","s",$no_sa_system);
+        if (!$no_ie)
+            brow("Edge",$u_ie,"Microsoft","i",$no_ie_system);        
+        ?>
     </tr>
 </table>
 <?php
+
 /*
-function display_browser($name, $url,$logo="",$in_countries=False,min_systems=False) {
-	global $lr
-	return '<a href="'.$url.'">'.$name.'</a>'// sprintf("/?lang=de",$lr);
-	//<a href="<?php sprintf("/?lang=de",$lr);?>
-}
-display_browser("Yandex Browser", "https://browser.yandex.com")
+display_browser("Yandex Browser", "https://browser.yandex.com",)
 display_browser("Seamonkey", "http://www.seamonkey-project.org/releases/#2.33")
 display_browser("Maxthon", "http://maxthon.com")
 display_browser("Vivaldi", "https://vivaldi.com/")
-
-
 */
+
+
 ?>
-
-
- </p>
-    
-<h2 class="whatnow"> 
+<h2 class="whatnow">
     <?php echo sprintf(T_('For more <a href="%s">security</a>,    <a href="%s">speed</a>,    <a href="%s">comfort</a> and    <a href="%s">fun</a>.'),'#security','#speed','#comfort','#fun');?>
 </h2>
 </div>
 
 <?php
+}
 include("ads.php");
- 
+
 if (false) {
     //for translations
-?>
-<div class="example">
-        <?php echo sprintf(T_('Your browser (%s) is <b>out of date</b>. ' .
-        'It has known <b>security flaws</b> and may <b>not ' .
-        'display all features</b> of this and other websites. ' .
-        '<a%s>Learn how to update your browser</a>'),'Internet Explorer 6',' href="update.html"');?>
-</div>
-<?php
+    sprintf(T_('This website would like to remind you: Your browser (%s) is <b>out of date</b>. <a%s>Update your browser</a> for more security, comfort and the best experience on this site.'),'Internet Explorer 6',' href="update.html"');
+    //sprintf(T_('Your browser (%s) is <b>out of date</b>. It has known <b>security flaws</b> and may <b>not display all features</b> of this and other websites. <a%s>Learn how to update your browser</a>'),'Internet Explorer 6',' href="update.html"');
 }
 ?>
 
@@ -173,45 +155,40 @@ if (false) {
 
 <div>
     <h2><?php echo T_('Why do I need an up-to-date browser?'); ?></h2>
-        <div id="advc">
-                <ul class="advantages">
-                        <li class="security" id="security">
-                                <h3><?php echo T_('Security'); ?></h3>
-                                <div><?php echo T_('Newer browsers protect you better against scams, viruses, trojans, phishing and other threats. They also fix security holes in your current browser!'); ?></div>
-                        </li>
-                        <li class="speed" id="speed">
-                                <h3><?php echo T_('Speed'); ?></h3>
-                                <div><?php echo T_('Every new browser generation improves speed'); ?></div>
-                        </li>
-                        <li class="compatibility" id="fun">
-                                <h3><?php echo T_('Compatibility'); ?></h3>
-                                <div><?php echo T_('Websites using new technology will be displayed more correctly'); ?></div>
-                        </li>
-                        <li class="comfort" id="comfort">
-                                <h3><?php echo T_('Comfort &amp; better experience'); ?></h3>
-                                <div><?php echo T_('With new features, extensions and better customisability, you will have a more comfortable web-experience'); ?></div>
-                        </li>
-                </ul>
-        </div>
-    
-        <div>
-                <h2><?php echo T_('Why this website?'); ?></h2>
-                <p>
-                        <?php echo T_('This website is an initiative by webdesigners, webmasters and bloggers who want to bring the web further and help their visitors.'); ?>
-                </p>
-                <p>
-                        <?php echo T_('Outdated browsers are a <b>security threat</b> and are <b>blocking the advancement of the web</b> because of their limited features and many <b>bugs</b>.'); ?>
-                </p>
-                <p>
-                    <a href="./"><?php echo T_('More information about the Project Browser-Update.org'); ?></a>
-                </p>
-        </div>
+    <div id="advc">
+        <ul class="advantages">
+            <li class="security" id="security">
+                <h3><?php echo T_('Security'); ?></h3>
+                <div><?php echo T_('Newer browsers protect you better against viruses, scams and other threats. Outdated browsers have security holes which are fixed in updates.')?></div>
+            </li>
+            <li class="speed" id="speed">
+                <h3><?php echo T_('Speed'); ?></h3>
+                <div><?php echo T_('Every new browser generation improves speed')?></div>
+            </li>
+            <li class="compatibility" id="fun">
+                <h3><?php echo T_('Compatibility & new Technology')?></h3>
+                <div><?php echo T_('You can view sites that are using the latest technology')?></div>
+            </li>
+            <li class="comfort" id="comfort">
+                <h3><?php echo T_('Comfort & better experience')?></h3>
+                <div><?php echo T_('Have a more comfortable experience with new features, extensions and better customisability.')?></div>
+            </li>
+        </ul>
+    </div>
+
+    <div>
+        <h2><?php echo T_('Why this website?')?></h2>
+        <p>
+            <?php echo T_('This is an initiative by websites and blogs to raise security awareness and bring forward the web.'); ?>
+            <a href="./"><?php echo T_('About the Project')?></a>
+        </p>
+    </div>
 
 
-        <h2><?php echo T_('"I\'m not able to update my browser"'); ?></h2>
-        <p><?php echo T_('If you can\'t change your browser because of compatibility issues, think about installing a second browser for browsing and keep the old one for the compatibility.'); ?></p>
-        <p><?php echo T_('If you are on a computer that is maintained by an admin and you cannot install a new browser, ask your admin about it.'); ?></p>
- 
+    <h2><?php echo T_('I can\'t update my browser')?></h2>
+    <p><?php echo T_('If you can\'t change your browser because of compatibility issues, think about installing a second browser for browsing and keep the old one for compatibility.'); ?></p>
+    <p><?php echo T_('Ask your admin to update your browser if you cannot install updates yourself.')?></p>
+
 </div>
 
 
@@ -220,18 +197,7 @@ var cv=3;
 var second=false;
 countView();
 </script>
-<script type="text/javascript" src="//cdn.jsdelivr.net/cookie-bar/1/cookiebar-latest.min.js?tracking=1&thirdparty=1"></script> 
-<style>
-    #cookie-bar {
-        background: #ddd;
-    }
-    #cookie-bar p {
-        color: #777;
-    }    
-#cookie-bar-button {
-    background-color: #999;
-    border-bottom: none;
-    text-shadow: none;
-}
-</style>
-<?php include("footer.php");?>
+<div class="cookiebar"><?php echo T_('This website uses cookies')?></div>
+<?php 
+include("footer.php");
+?>
