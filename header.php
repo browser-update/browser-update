d470effd77da251657a0839baa4676fbb79d5781
josselex <jossele@gmx.de>
2016-08-21 19:06:11 +0200

    renamed base.css

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
 
     <meta name="robots" content="index,follow" />
     <link href='//fonts.googleapis.com/css?family=Open+Sans:400,300&subset=latin,latin-ext,cyrillic,greek' rel='stylesheet' type='text/css'/>
-    <link rel="stylesheet" href="/base.css" type="text/css" />
+    <link rel="stylesheet" href="/basenew.css" type="text/css" />
     <!--[if lte IE 7]>
     <link rel="stylesheet" href="/drecksie.css" type="text/css" />
     <![endif]-->
@@ -93,20 +93,6 @@
 </div>
 
 <div id="co">
-<!--
-<div id="bookmark">
-	<div class="addthis_toolbox addthis_default_style addthis_32x32_style">
-	<a class="addthis_button_preferred_1"></a>
-	<a class="addthis_button_preferred_2"></a>
-	<a class="addthis_button_preferred_3"></a>
-	<a class="addthis_button_preferred_4"></a>
-	<a class="addthis_button_compact"></a>
-	</div>
-	<script type="text/javascript">var addthis_config = {"data_track_clickback":true};</script>
-	<script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=Jossele"></script>
-</div>
-<div id="body">
--->
 <?php
 if (!isset($extratranslation))  {
     T_textdomain('site');
