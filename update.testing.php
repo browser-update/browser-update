56abf74802736d93f2b5a703425254554eadfb18
josselex <jossele@gmx.de>
2017-01-09 09:00:17 +0100

    more

--- update.testing.php
+++ update.testing.php
@@ -1,9 +1,11 @@
 <?php
 T_textdomain('site');
-echo '<div class="testwarning">'.
-    T_('This is the page that is shown to people with an outdated browser when they get the notification to update their browser.').
-    ' ' .T_('Your current browser may actually be up-to-date.'). ' ' . 
-    '<a class="greenbut" href="update-browser.html">'.
-    T_('Check if my browser is up-to-date!').
-'</a></div>';
+if (is_translated("Check if my browser is up-to-date!")) {
+    echo '<div class="testwarning">'.
+        T_('This is the page that is shown to visitors with an out-of-date browser after they clicked the notification to update their browser.').
+        ' ' .T_('This is not the information for your currently used browser.'). ' ' . 
+        '<a class="greenbut" href="update-browser.html">'.
+        T_('Check if my browser is up-to-date!').
+    '</a></div>';
+}
 T_textdomain('update');
