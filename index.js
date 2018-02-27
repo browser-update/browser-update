6335e5196b6de9194bdb54a7d5166c1be7e4c68f
Сергей Ермаков <podkot@gmail.com>
2018-02-27 20:25:16 +0300

    /index.js, config & runner file

---
+++ index.js
@@ -0,0 +1,39 @@
+import browserUpdate from './update.npm.full.js';
+
+const OUTDATE_DIFF = -0.01;
+
+const settings = {
+
+    // Уведомляемые версии браузеров. Отрицательное число означает отсчет версий от текущей.
+    notify : {
+        i : OUTDATE_DIFF,
+        f : OUTDATE_DIFF,
+        o : OUTDATE_DIFF,
+        s : OUTDATE_DIFF,
+        c : OUTDATE_DIFF,
+    },
+
+    // через сколько часов сообщение появится снова
+    reminder : 1,
+
+    // Проверять уязвимости в браузера
+    insecure : true,
+
+    unsupported : true,
+
+    // открыть ссылку в новом окне/вкладке
+    newwindow : true,
+
+    // Не показывать кнопку "Игнорировать" для закрытия уведомления
+    noclose : true,
+
+    // DOM Element where the notification will be injected.
+    container : document.body,
+
+    // The position where the notification should be shown. Available options are:"top", "bottom", "corner"
+    style : 'bottom',
+};
+
+document.addEventListener( 'DOMContentLoaded', function() {
+    browserUpdate( settings );
+} );
