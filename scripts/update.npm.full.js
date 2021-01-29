caf738a41d2d975fdc19220ed113a8a74a31468f
Ryan Mark <rgmark@gmail.com>
2021-01-29 08:26:28 -0800

    [#176604059] Outdated/unsupported browser banner for Telehealth

--- scripts/update.npm.full.js
+++ scripts/update.npm.full.js
@@ -5,7 +5,7 @@
 var $bu_= new function() {
     var s=this;
     this.version="3.3.26";
-    this.vsakt = {c:"87.0.4280",f:83,s:"13.1.2",e:86,i:12,ios:"13.3",samsung:12.0,o:71,e_a:45.09,o_a:59.1,y:"20.9.1",v:3.3,uc:"13.3"};
+    this.vsakt = {c:"88.0.4324.96",f:84,s:"14.0.2",e:88,i:12,ios:"14.0",samsung:12.0,o:71,e_a:45.09,o_a:59.1,y:"20.9.1",v:3.3,uc:"13.3"};
     //severely insecure below(!) this version, insecure means remote code execution that is actively being exploited
     this.vsinsecure_below = {c:"86.0.4240.111",f:76,s:"11.1.1",e:16,i:11,ios:"12.3",samsung:9.0,o:62,o_a:52,y:"20",v:"2.7",uc:"13.1"};
     this.vsdefault = {c:-3,f:-3,s:-1,e:17,i:11,ios:10,samsung:9.9,o:-3,o_a:-3,y:20.4,v:2.6,uc:13.0,a:535};
@@ -53,7 +53,7 @@ var $bu_= new function() {
             r.n=pats[i][1];
             r.engine=pats[i][2];
             break;
-        }        
+        }
     }
     r.fullv=RegExp.$1;
     r.v=parseFloat(r.fullv);
@@ -138,11 +138,11 @@ var $bu_= new function() {
     }
     r.t=s.names[r.n]+" "+r.v;
     r.is_supported=r.is_latest= !s.vsakt[r.n] ? undefined : s.less(r.fullv,s.vsakt[r.n])<=0;
-    
+
     r.vmaj=Math.round(r.v);
 
     r.is_insecure= r.is_insecure|| !s.vsinsecure_below[r.n] ? undefined :  s.less(r.fullv,s.vsinsecure_below[r.n])===1;
-    
+
     if ((r.n==="f" && (r.vmaj===78)) || (r.n==="i" && r.vmaj===11)) {
         r.is_supported=true;
         r.is_insecure=false;
@@ -168,7 +168,7 @@ var $bu_= new function() {
     if (r.engine) {
         ua.match(new RegExp(engines[r.engine].replace("VV",VV),"i"))
         r.engine_version=parseFloat(RegExp.$1)
-    }    
+    }
     return r
 }
 this.semver = function(vstr) {
