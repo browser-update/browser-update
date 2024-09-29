f64f7216a80e564d085eddbce9cf0093eb80d42f
josselex <jossele@gmx.de>
2024-09-29 19:07:44 +0200

    update versions

--- scripts/update.npm.full.js
+++ scripts/update.npm.full.js
@@ -4,10 +4,10 @@
 
 var $bu_= new function() {
     var s=this;
-    this.version="3.3.53npm";
-    this.vsakt = {c:"125",f:"126",s:"17.4.1",e:"124",i:"12",ios:"17.4.1",samsung:"25",o:"109",e_a:"123",o_a:"80",y:"23.9.1",v:"6.6",uc:"13.6.8"};
+    this.version="3.3.54npm";
+    this.vsakt = {c:"128",f:"130",s:"17.6",e:"129",i:"12",ios:"17.6",samsung:"27",o:"114",e_a:"128",o_a:"84",y:"24.7.8",v:"6.9",uc:"13.7.8"};
     //severely insecure below(!) this version, insecure means remote code execution that is actively being exploited
-    this.vsinsecure_below = {c:"119",f:"115",s:"11.1.1",e:"119",i:11,ios:"15.8.2",samsung:12.0,o:62,o_a:78,y:"20",v:"6.0",uc:"13.4"};
+    this.vsinsecure_below = {c:"126",f:"126",s:"11.1.1",e:"119",i:11,ios:"16.5",samsung:12.0,o:62,o_a:78,y:"20",v:"6.0",uc:"13.4"};
     this.vsdefault = {c:-3,f:-3,s:-2,e:17,i:11,ios:12,samsung:-3,o:-3,o_a:-3,y:-1,v:-1,uc:-0.2,a:535};
     this.names={c:"Chrome",f:'Firefox',s:'Safari',e:"Edge",i:'Internet Explorer',ios:"iOS",samsung:"Samsung Internet",o:'Opera',o_a:'Opera', e_a:"Edge", y:"Yandex Browser",v:"Vivaldi",uc:"UC Browser",a:"Android Browser",x:"Other",silk:"Silk"};
 
@@ -128,7 +128,7 @@ var $bu_= new function() {
 
     r.is_insecure= r.is_insecure|| !s.vsinsecure_below[r.n] ? undefined :  s.less(r.fullv,s.vsinsecure_below[r.n])===1;
     
-    if ((r.n==="f" && (r.vmaj===115)) || (r.n==="c" && (r.vmaj===120))) {
+    if ((r.n==="f" && (r.vmaj===115 || r.vmaj===128)) || (r.n==="c" && (r.vmaj===120))) {
         r.is_supported=true;
         r.is_insecure=false;
         if (r.n==="f")
